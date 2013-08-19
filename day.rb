505b63b84f2106ec910b563f1ac2840f611e8b57
Cameron Carroll <ckckarroll4@gmail.com>
2013-08-18 23:37:52 -0700

    Complete rewrite to use classes and support time expectations

--- day.rb
+++ day.rb
@@ -10,267 +10,325 @@ require 'bundler/setup'
 require 'trollop'
 require 'yaml'
 require 'fileutils'
+require 'pry'
 
-VERSION = '0.3'
+VERSION = '0.9'
 CONFIG_FILE = ENV['HOME'] + '/.app_data/.daytodo'
 HISTORY_FILE = ENV['HOME'] + '/.app_data/.daytodo_history'
 
-def parse_options
-	opts = Trollop::options do
-		version "day.rb #{VERSION} (c) 2013 Cameron Carroll"
-		banner <<-EOS
+#-------------- Monkey Classes:
 
-day.rb is a time-tracking/to-do app. It allows tasks to be defined for certain days, or everyday.
-It is intended to help keep you organized, and allow quick and unobtrusive context switching.
-
-
-Usage:
-
-  Simply select a task to begin timing it, and run the same command again to stop.
-
-  day.rb [options] [task number]
-
-  Examples:
-
-  # day.rb -- List tasks for the day.
-  # day.rb new_task -- Adds a new task; Defaults to 'every day.'
-  # day.rb 0 -- Switch context & start timing new_task... tasks are indexed from 0.
-  # day.rb new_task2
-  # day.rb 1 -- Switch context and save time spent in previous context.
-  # day.rb 1 -- Exit context, save times.
-
-  #day.rb new_task3 m w thu -- Create new_task3 for monday, wednesday and thursday.
-
-  Notes:
-
-  # Days of the week can either be defined as individual letters and digraphs,
-    where necessary, or trigraphs where desired. Must be given as a list of lowercase keys.
-    ie: m, tu, w, th, f, sa, su
-    You could also use the internal representation, where 0 corresponds to sunday and 6 to saturday.
-
-  # When using day.rb <noun>, integer numerical input will switch context,
-    while alphanumeric input will create a new task.
-
-EOS
-		opt :new, "Add a new task."
-		opt :name, "Name for new task.", :type => :string
-		opt :days, "Days to enable task", :type => :strings
-	end
-
-	Trollop::die :days, "Must select --new flag to specify days" if opts[:days] && !opts[:new]
-  Trollop::die :name, "Must select --new flag to specify name" if opts[:name] && !opts[:new]
-  Trollop::die :name, "Must specify a --name for the task" if opts[:days] && !opts[:name]
-
-	return opts
+class String
+  def nan?
+    self !~ /^\s*[+-]?((\d+_?)*\d+(\.(\d+_?)*\d+)?|\.(\d+_?)*\d+)(\s*|([eE][+-]?(\d+_?)*\d+)\s*)$/
+  end
 end
 
+#-------------- App Classes:
 
-class DayList
-  attr_accessor :config_data, :history_data, :config_path, :history_path, :current_context  ,
-                :context_entrance_time, :valid_tasks
 
-  def initialize(config_filename, config_history_filename)
-    @config_path = config_filename
-    @history_path = config_history_filename
-    FileUtils.mkdir_p(File.dirname(@config_path))
-    generate_configuration unless File.exists? @config_path
-    generate_history unless File.exists? @history_path
-    @config_data = load_configuration
-    @valid_tasks = load_valid_tasks
-    @current_context = @config_data[:current_context]
-    @context_entrance_time = @config_data[:context_entrance_time]
-    @history_data = load_history
-  end
+class List
 
-  def generate_history
-    puts "[NOTICE] Couldn't find history file (~/.TODO_HISTORY) -- Generating a new one."
-    stub_history = {
-      :VERSION => VERSION,
-      :task_history => {}
-    }
-    save_yaml_data(stub_history, @history_path)
-  end
+  attr_accessor :tasks, :current_context, :context_entrance_time
 
-  def generate_configuration
-    puts "[NOTICE] Couldn't find config file (~/.TODO) -- Generating a new one."
-    stub_config = {
-      :version => VERSION,
-      :tasks => []
-    }
-    save_yaml_data(stub_config, @config_path)
-  end
-
-  def load_valid_tasks
-    valid_tasks = []
-    unless @config_data[:tasks].empty?
-      @config_data[:tasks].each do |task|
-        valid_tasks << task if valid_task? task
+  def initialize(task_list, current_context, context_entrance_time)
+    @tasks = {}
+    if task_list
+      task_list.each do |task|
+        #task.first refers to the key (task name), since the task is stored [key, val]
+        task_instance = Task.new(task.first, task[1][:days], task[1][:commitment])
       end
     end
 
-    return valid_tasks
+    @current_context = current_context if current_context
+    @context_entrance_time = context_entrance_time if context_entrance_time
   end
 
-  def valid_task?(task)
-    if task[:days]
+  def addTask(name, days, commitment)
+    this_task = Task.new(name, days, commitment)
+    @tasks[name] = this_task
+  end
+end
+
+class Task
+
+  attr_reader :name, :valid_days, :time_commitment
+
+  def initialize(name, valid_days, time_commitment)
+    @name = name
+    @valid_days = valid_days
+    @time_commitment = time_commitment
+  end
+
+  def self.valid_today?
+    if @valid_days
       today = Time.new.wday #0 is sunday, 6 saturday
 
       weekday_short = case today
-        when 0 then 'su'
-        when 1 then 'm'
-        when 2 then 'tu'
-        when 3 then 'w'
-        when 4 then 'th'
-        when 5 then 'f'
-        when 6 then 'sa'
+      when 0 then 'su'
+      when 1 then 'm'
+      when 2 then 'tu'
+      when 3 then 'w'
+      when 4 then 'th'
+      when 5 then 'f'
+      when 6 then 'sa'
       end
 
       weekday_long = case today
-        when 0 then 'sun'
-        when 1 then 'mon'
-        when 2 then 'tue'
-        when 3 then 'wed'
-        when 4 then 'thu'
-        when 5 then 'fri'
-        when 6 then 'sat'
+      when 0 then 'sun'
+      when 1 then 'mon'
+      when 2 then 'tue'
+      when 3 then 'wed'
+      when 4 then 'thu'
+      when 5 then 'fri'
+      when 6 then 'sat'
       end
 
-      if task[:days].include?(today) || task[:days].include?(weekday_short)
+      if @valid_days.include?(today) || @valid_days.include?(weekday_short)
         return true
-      elsif task[:days].include? weekday_long || task[:days].empty?
+      elsif @valid_days.include? weekday_long || @valid_days.empty?
         return true
       else
         return false
       end 
     else
-      return true
+      return true # valid everyday
     end
   end
-
-
-
-  def load_history
-    load_yaml_data(@history_path)
-  end
-
-  def save_history
-    save_yaml_data(@history_data, @history_path)
-  end
-
-  def load_configuration
-    load_yaml_data(@config_path)
-  end
-
-  def save_configuration
-    @config_data[:current_context] = @current_context
-    @config_data[:context_entrance_time] = @context_entrance_time
-    save_yaml_data(@config_data, @config_path)
-  end
-
-  def load_yaml_data(filename)
-    config_data = File.open(filename, 'r') { |handle| load = YAML.load(handle) }
-    config_data = {} unless config_data
-    return config_data
-  end
-
-  def save_yaml_data(data, filename)
-    File.new(filename, "w") unless File.exist? filename
-    File.open(filename, "w") do |yaml_file|
-      yaml_file.write(data.to_yaml)
-    end
-  end
-
-  def print
-    puts ""
-    puts "Day:"
-    counter = 0
-    @valid_tasks.each do |task|
-      puts counter.to_s + ': ' + task[:name]
-      counter = counter + 1
-    end
-    puts ""
-    if @current_context
-      puts "Current Context: " + find_task_name(@current_context)
-      print_context_time
-    end
-    puts ""
-  end
-
-  def print_context_time
-    time = "%0.2f" % ((Time.new.getutc - @context_entrance_time) / 60)
-    puts "Time spent in context: " + time.to_s + " minutes."
-  end
-
-  def create_task(name, days)
-    puts "Adding new task: #{name}" 
-    @config_data[:tasks] << { :name => name, :days => days}
-    self.save_configuration
-  end
-
-  def find_task_name(numeric_selection)
-    if @valid_tasks[numeric_selection.to_i]
-      return @valid_tasks[numeric_selection.to_i][:name]
-    else
-      return nil
-    end
-  end
-
-  def enter_context(numeric_selection)
-    leave_context if @current_context
-    @current_context = numeric_selection
-    @context_entrance_time = Time.now.getutc
-    self.save_configuration
-  end
-
-  def leave_context
-    print_context_time
-    task_name = find_task_name(@current_context)
-    @history_data[:task_history][task_name] = Array.new unless @history_data[:task_history][task_name]
-    @history_data[:task_history][task_name] << [@context_entrance_time, Time.now.getutc]
-    @current_context, @context_entrance_time = nil, nil
-    save_history
-    save_configuration
-  end 
-
 end
 
-def main
-  puts ""
-	opts = parse_options
-  list = DayList.new(CONFIG_FILE, HISTORY_FILE)
-  if !opts[:new] && ARGV.empty?
-    list.print
-  elsif opts[:new] && !opts[:name] && !opts[:days]
-    # call new task wizard
-  elsif opts[:new] && opts[:name] && !opts[:days]
-    list.create_task(opts[:name], nil)
-  elsif opts[:new] && opts[:name] && opts[:days]
-    list.create_task(opts[:name], opts[:days])
+
+class BaseConfig
+
+  attr_reader :file_path, :data
+
+  def initialize(file_path)
+    @file_path = file_path
+    generate unless File.exists? @file_path
+    load
+  end
+
+  def load
+    @data = load_hash_from_yaml
+  end
+
+  def save(data)
+    save_hash_to_yaml(data)
+  end
+
+  private
+
+  def generate
+    binding.pry
+    puts "[Notice:] Couldn't find '#{@file_path}' -- Generating a new one."
+    stub = {
+      :version => VERSION,
+      :tasks => {}
+    }
+    save_hash_to_yaml(stub)
+  end
+
+  def save_hash_to_yaml(hash)
+    FileUtils.mkdir_p(File.dirname(@file_path))
+    File.new(@file_path, "w") unless File.exist? @file_path
+    File.open(@file_path, "w") do |yaml_file|
+      yaml_file.write(hash.to_yaml)
+    end
+  end
+
+  def load_hash_from_yaml
+    yaml_data = File.open(@file_path, 'r') { |handle| load = YAML.load(handle) }
+    yaml_data = {} unless yaml_data
+    return yaml_data
+  end
+end
+
+class Configuration < BaseConfig
+  attr_accessor :tasks
+
+  def initialize(file_path)
+    super(file_path)
+  end
+
+  def load
+    super
+    @tasks = []
+    unless @data[:tasks].empty?
+      binding.pry
+      @data[:tasks].each do |task|
+        task_object = Task.new(task.first, task[1][:days], task[1][:commitment])
+        @tasks << task_object
+      end
+    end
+  end
+
+  def save_task(task, valid_days, time_commitment)
+    @data[:tasks][task] = {:days => valid_days, :commitment => time_commitment}
+    binding.pry
+    save(data)
+  end
+end
+
+class History < BaseConfig
+  def initialize(file_path)
+    super(file_path)
+  end
+end
+
+#-------------- Loose methods:
+
+def parse_options
+  opts = {}
+
+  # Check first argument, which defines behavior.
+  # We can :print, :commit, select a :chosen_context,
+  # or define a :new_task.
+  case ARGV[0]
+  when nil
+    opts[:print] = true
+  when 'commit'
+    opts[:commit] = true
   else
-    if ARGV.first.numeric?
-      if ARGV.first == list.current_context
-        puts "Leave context: " + list.find_task_name(ARGV.first)
-        list.leave_context
-      else
-        if list.config_data[:tasks].size-1 >= ARGV.first.to_i
-          puts "Enter context: " + list.find_task_name(ARGV.first)
-          list.enter_context(ARGV.first)
+    # Argument doesn't match any commands...
+    # So we assume it's a new task definition if alphanumeric,
+    # and assume we want to switch context if numeric.
+    if !ARGV[0].nan?
+      opts[:chosen_context] = ARGV[0]
+    else
+      opts[:new_task] = ARGV[0]
+    end
+  end
+
+  # When we define a new task we can specify the days and time inline.
+  # For each additional argument, if it's numeric, assume we're specifying the time.
+  # If it's alpha, check it against our list of monographs/digraphs/etc
+  if opts[:new_task]
+    args = ARGV[1..-1]
+    opts[:valid_days] = true if args
+    args.each do |arg|
+      arg = arg.downcase
+      if arg.nan?
+        key = parse_day_argument(arg)
+        if opts[key]
+          raise 'Cannot specify a single day more than one time!'
         else
-          puts "Invalid selection..."
-          list.print
+          opts[key] = true
+        end
+      else
+        if opts[:time]
+          raise 'Can only supply one numerical argument corresponding to commitment time!'
+        else
+          opts[:time] = arg
         end
       end
-      
-    else
-      list.create_task(ARGV.first, nil)
     end
+  end
+
+  # If commit is true, we have to supply some other arguments:
+  # Task name and expected time commitment.
+  if opts[:commit]
+    # Check for new task definition.
+    # Check that ARG 1 exists and that it is NOT numeric.
+    task_missing_error = "Must supply task name after 'commit' keyword. (i.e. 'day commit read_hn 0.5')"
+    if ARGV[1]
+      if ARGV[1].nan?
+        opts[:task_name] = ARGV[1]
+      else
+        raise task_missing_error
+      end
+    else
+      raise task_missing_error
+    end
+
+    # Check for time commitment definition.
+    # Check that ARG 2 exists and that it IS numeric.
+    time_missing_error = "Must supply expected time commitment after task name. (i.e. 'day commit read_hn 0.5')"
+    if ARGV[2]
+      if ARGV[2].nan?
+        raise time_missing_error
+      else
+        opts[:time_commitment] = ARGV[2]
+      end
+    else
+      raise time_missing_error
+    end
+  end
+
+  return opts
+end
+
+def parse_day_argument(day)
+  case day
+  when 'su', 'sun', 'sunday', '0'
+    return :sunday
+  when 'm', 'mo', 'mon', 'monday', '1'
+    return :monday
+  when 'tu', 'tue', 'tues', 'tuesday', '2'
+    return :tuesday
+  when 'w', 'we', 'wed', 'wednesday', '3'
+    return :wednesday
+  when 'th', 'thu', 'thur', 'thurs', 'thursday', '4'
+    return :thursday
+  when 'f', 'fr', 'fri', 'friday', '5'
+    return :friday
+  when 'sa', 'sat', 'satu', 'satur', 'saturday', '6'
+    return :saturday
+  else
+    raise 'Could not parse day argument! Check your day glyphs.'
   end
 end
 
-class String
-  def numeric?
-       !!(self =~ /^[-+]?[0-9]+$/)
+def parse_day_keys(opts)
+  days = []
+  opts.each do |key, value|
+    continue unless value
+    case key
+    when :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday
+      days << key
     end
+  end
+  return days
+end
+
+#-------------- Main control method:
+
+def main
+
+  opts = parse_options
+
+  if opts[:switch]
+    hist = History.new(HISTORY_FILE)
+    history_data = hist.load
+  end 
+
+  config = Configuration.new(CONFIG_FILE)
+  config_data = config.data
+
+  # Generate list from configuration data:
+  list = List.new(config_data[:tasks], config_data[:current_context], config_data[:context_entrance_time]);
+
+
+  # Handle behaviors:
+  if opts[:print]
+    puts 'print'
+  elsif opts[:chosen_context]
+    puts 'context switch'
+  elsif opts[:new_task]
+    if opts[:valid_days]
+      valid_days = parse_day_keys(opts)
+    else
+      valid_days = nil
+    end
+    puts 'okay now we save the task'
+    config.save_task(opts[:new_task], valid_days, opts[:time])
+  elsif opts[:commit]
+    puts 'commitment'
+  else
+    raise "No behavior defined! Check options parsing. "
+  end
 end
 
 main()
+
+
+
