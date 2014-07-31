aca50f435c3d00b29f0cec48e4f53e32a0dde6fd
Cameron Carroll <ckcarroll4@gmail.com>
2014-07-31 15:12:22 -0700

    Move bootstrap code to spec_helper, gen test task

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 require 'fileutils'
 describe Configuration do
-	FILE_PATH = 'test_db'
-	FULL_FILE_PATH = "#{FILE_PATH}.db"
 
 	describe "#initialize" do
 		before :each do
@@ -29,18 +27,10 @@ describe Configuration do
 			valid_days = nil
 			estimate = nil
 			expect(@config.tasks[test_name]).to be(nil)
-			@config.save_task(test_name, test_description, valid_days,
-				estimate)
+			@config.save_task(test_name, test_description, valid_days, estimate)
 			@config.reload
 			expect(@config.tasks[test_name].name).to eq(test_name)
 		end
 	end
 
-end
-
-def bootstrap
-
-	FileUtils.rm FULL_FILE_PATH if File.exist? FULL_FILE_PATH
-	@db = YAML::DBM.new(FILE_PATH)
-	@config = Configuration.new(@db)
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,13 @@
 require_relative '../lib/configuration'
+require_relative '../lib/parser'
+
+FILE_PATH = 'test_db'
+FULL_FILE_PATH = "#{FILE_PATH}.db"
+
+def bootstrap
+	FileUtils.rm FULL_FILE_PATH if File.exist? FULL_FILE_PATH
+	@db = YAML::DBM.new(FILE_PATH)
+	@config = Configuration.new(@db)
+	@config.save_task("test", nil, nil, nil)
+	@config.reload
+end
